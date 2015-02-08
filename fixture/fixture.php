6e49a4172b40cfb15b7e53dfeea2de41b33f2716
Alexandre Ferreira <alexjrf22@gmail.com>
2015-02-07 23:22:45 -0200

    Correções Fixture

--- fixture/fixture.php
+++ fixture/fixture.php
@@ -8,31 +8,39 @@ $conexao = conexaoDB();
 
 echo "Removendo Tabela";
 
-$conexao->query("DROP TABLE IF EXISTS teste");
+$conexao->query("DROP TABLE IF EXISTS paginas");
 echo " - OK\n";
 
 echo "Criando tabela";
 $conexao->query
         (
-            "CREATE TABLE teste("
+            "CREATE TABLE paginas("
             . "id INT NOT NULL AUTO_INCREMENT,"
-            . "nome VARCHAR(45) CHARACTER SET 'utf8' NULL,"
+            . "pagina VARCHAR(45) CHARACTER SET 'utf8' NULL,"
+            . "conteudo VARCHAR(255) CHARACTER SET 'utf8' NULL,"
             . "PRIMARY KEY(id));"
             
         );
+
 echo " - OK\n";
 
-echo 'Inserindo Dados';
+echo "Inserindo Dados";
 
-for ($x=0; $x <= 9; $x++)
-{
-    $nome = "Teste {$x}";
-    $sql = $conexao->prepare("INSERT INTO teste (nome) VALUES (:nome);");
-    $sql->bindParam(":nome", $nome);
-    $sql->execute();
-}
 
+$array = array("Home", "Empresa", "Produtos", "Serviços");
+    
+    for($x=0; $x<=3; $x++)
+    {
+        $pagina = $array[$x];
+        $conteudo = $array[$x];
+        $sql = $conexao->prepare("INSERT INTO paginas (pagina, conteudo) VALUES (:pagina, :conteudo);");
+        $sql->bindParam(":pagina", $pagina);
+        $sql->bindParam(":conteudo", $conteudo);
+        $sql->execute(); 
+    }
+    
 echo " - OK\n";
 
 echo "#### Concluído ####\n";
         
+      
